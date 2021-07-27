fc0296be4729c8e8c03f561876160ba25682e280
lijanalm <72161653+lijanalm@users.noreply.github.com>
2021-07-27 22:27:05 +0300

    Update 1_rank.sql

--- 1_rank.sql
+++ 1_rank.sql
@@ -1,4 +1,5 @@
 #https://www.sqlshack.com/overview-of-sql-rank-functions/ 
+#--------------------------------
 
 #CSV file created and uploaded in location specified
 CREATE TABLE ExamResults
@@ -9,15 +10,13 @@ CREATE TABLE ExamResults
    ROW FORMAT DELIMITED
     FIELDS TERMINATED BY ','
     LOCATION '/user/training/ExamResults';
-
-
+#--------------------------------
 
 #ROW_Number: It assigns the sequential rank number to each unique record.
 SELECT studentname, subject, marks,
      row_number() OVER(ORDER BY marks) AS RowNumber
     FROM examresults;
-
-
+#--------------------------------
 
 #RANK: It assigns the rank number to each row in a partition. It skips the number for similar values.
 SELECT studentname, subject, marks,
@@ -25,8 +24,7 @@ SELECT studentname, subject, marks,
     FROM examresults
     ORDER BY studentname, Rank;
 #PARTITION BY Studentname clause performs calculations on each student group
-
-
+#--------------------------------
 
 #Dense_RANK: It assigns the rank number to each row in a partition. It does not skip the number for similar values.
 SELECT studentname, subject, marks,
@@ -38,8 +36,7 @@ SELECT studentname, subject, marks,
      dense_rank() OVER(PARTITION BY subject ORDER BY marks DESC) AS Rank
     FROM examresults
     ORDER BY subject, Rank;
-
-
+#--------------------------------
 
 #NTILE(N): It divides the number of rows as per specified partition and assigns unique value in the partition.
 SELECT *, ntile(2) OVER(ORDER BY marks DESC) AS rank
