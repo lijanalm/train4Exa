e944ab8c32eee571210b06a4e2e83fb5f0bef6d6
lijanalm <72161653+lijanalm@users.noreply.github.com>
2021-07-27 15:55:17 +0300

    Update rank.sql

--- rank.sql
+++ rank.sql
@@ -14,7 +14,21 @@ SELECT studentname, subject, marks,
      row_number() OVER(ORDER BY marks) AS RowNumber
     FROM examresults;
 
-#RANK: It assigns the rank number to each row in a partition. It skips the number for similar values.
 
-    
+#RANK: It assigns the rank number to each row in a partition. It skips the number for similar values.
+SELECT studentname, subject, marks,
+     rank() OVER(PARTITION BY studentname ORDER BY marks DESC) AS Rank
+    FROM examresults
+    ORDER BY studentname, Rank;
+#PARTITION BY Studentname clause performs calculations on each student group
+
+
+#Dense_RANK: It assigns the rank number to each row in a partition. It does not skip the number for similar values.
+SELECT studentname, subject, marks,
+     dense_rank() OVER(ORDER BY marks DESC) AS Rank
+    FROM examresults
+    ORDER BY Rank;
+
+
+#NTILE(N): It divides the number of rows as per specified partition and assigns unique value in the partition.
  
