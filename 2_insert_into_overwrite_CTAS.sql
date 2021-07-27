94b71e0c23fa5d235bd165552b23163a705e6dc0
lijanalm <72161653+lijanalm@users.noreply.github.com>
2021-07-27 22:25:55 +0300

    Update and rename insert_into_overwrite_CTAS.sql to 2_insert_into_overwrite_CTAS.sql

--- 2_insert_into_overwrite_CTAS.sql
+++ 2_insert_into_overwrite_CTAS.sql
@@ -1,25 +1,26 @@
 #more about: https://www.coursera.org/learn/cloud-storage-big-data-analysis-sql/supplement/UV25d/sql-insert-statements 
+#--------------------------------
 
 #insert into (bad practice because of small files problem)
-#sintakse:
+#syntax:
 INSERT INTO tablename 
   VALUES
   (row1col1value,row1col2value, … ),
   (row2col1value,row2col2value, … ),
   … ;
-
+#--------------------------------
 
 #insert overwrite
-#sintakse:
+#syntax:
 INSERT OVERWRITE tablename SELECT * FROM tablename;
 
 INSERT OVERWRITE chicago_employees
   SELECT * FROM employees WHERE office_id='b';
-
+#--------------------------------
 
 #CTAS: CTAS effectively combines a CREATE TABLE operation and an INSERT … SELECT operation into a single step. 
 #The column names and data types for the newly created table are determined based on the names and types of the columns queried in the SELECT statement.
-#sintakse:
+#syntax:
 CREATE TABLE chicago_employees AS
   SELECT * FROM employees WHERE office_id='b';
 
