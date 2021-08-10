da5d7596639171b18e86e07d8be6d78df2d354f1
lijanalm <72161653+lijanalm@users.noreply.github.com>
2021-08-10 16:42:16 +0300

    Update 5_table_partitioning.sql

--- 5_table_partitioning.sql
+++ 5_table_partitioning.sql
@@ -12,9 +12,23 @@ CREATE TABLE customers_by_country (cust_id STRING, name STRING)
 
 #!!!Partition column must not also appear in the list of all the other columns.
 #You can specify more than one partition column by using a comma-separated column list in the PARTITIONED BY clause
+-------------------------------
 
+#Loading Data with Dynamic Partition
+#Dynamic partitioning automatically defines partitions when you load the data, using the values in the partition column.
+#The partition columns must be the rightmost columns in the data you are inserting, and they must be in the same order as they appear in the PARTITION clause.
+INSERT OVERWRITE TABLE customers_by_country 
+  PARTITION(country)
+  SELECT cust_id, name, country FROM customers;
 
+#Hive includes a safety feature that prevents users from accidental dynamic partitioning. You can disable this safety feature in Hive by setting the property:
+SET hive.exec.dynamic.partition.mode=nonstrict;
+-------------------------------
 
+#Loading Data with Static Partitioning
+#In static partitioning you manually define the different partitions by using an ALTER TABLE … ADD PARTITION statement, and then load the data into the partition. 
+ALTER TABLE customers_by_country ADD PARTITION (country='pk');
 
-
-
+INSERT OVERWRITE TABLE customers_by_country 
+  PARTITION(country='pk')
+  SELECT cust_id, name FROM customers WHERE country='pk'
